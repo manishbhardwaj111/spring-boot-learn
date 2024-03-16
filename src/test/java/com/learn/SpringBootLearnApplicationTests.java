9dd82cc2a13e37eb0476cba1c05e21e0f900bb41
Manish Bhardwaj <manishbhardwaj111@gmail.com>
2024-03-16 19:56:37 +0530

    Update SpringBootLearnApplicationTests.java

--- src/test/java/com/learn/SpringBootLearnApplicationTests.java
+++ src/test/java/com/learn/SpringBootLearnApplicationTests.java
@@ -3,11 +3,18 @@ package com.learn;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SpringBootLearnApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+   	 public void testAlwaysPasses() {
+        	assertTrue(true);
+    	}
 
 }
